8a14c4378fa883bea9efe423209728c250168226
Vikramacn5 <vikram.cn5@gmail.com>
2022-11-06 08:24:39 +0530

    Got a way to fetch the content from writer doc

--- fetch-content-injector.js
+++ fetch-content-injector.js
@@ -1,34 +1,36 @@
 console.log("from injector");
 
-const wait = (sec) => new Promise((res) => setTimeout(() => res(), sec));
+const delay = (sec) => new Promise((res) => setTimeout(() => res(), sec));
 
 let totalPage;
 
-const waitForElm = function (selector, shouldCheckText) {
+const waitForElm = function (selector, shouldCheckText, parent = document) {
   return new Promise(async (resolve, reject) => {
-    if (document.querySelector(selector)) {
+    if (parent.querySelector(selector)) {
+      await delay(1000);
       if (shouldCheckText) {
-        if (document.querySelector(selector).textContent != "") {
-          return resolve(document.querySelectorAll(selector));
+        if (parent.querySelector(selector).textContent != "") {
+          return resolve(parent.querySelectorAll(selector));
         }
       } else {
-        console.log(document.querySelectorAll(selector), "on load");
-        return resolve(document.querySelectorAll(selector));
+        console.log(parent.querySelectorAll(selector), "on load");
+        return resolve(parent.querySelectorAll(selector));
       }
     }
 
     const observer = new MutationObserver(async (mutations) => {
-      if (document.querySelector(selector)) {
+      if (parent.querySelector(selector)) {
+        await delay(1000);
         if (shouldCheckText) {
-          if (document.querySelector(selector).textContent != "") {
-            resolve(document.querySelectorAll(selector));
+          if (parent.querySelector(selector).textContent != "") {
+            resolve(parent.querySelectorAll(selector));
             observer.disconnect();
           }
         } else {
-          resolve(document.querySelectorAll(selector));
+          resolve(parent.querySelectorAll(selector));
           observer.disconnect();
         }
-        // await wait(2000);
+        // await delay(2000);
         // observer.disconnect();
       }
     });
@@ -45,8 +47,11 @@ window.addEventListener("load", async (e) => {
   totalPage = await waitForElm("#totalPage", true);
   totalPage = +totalPage[0].textContent.split(" ")[1];
   console.log(totalPage, "as a result");
-  await waitForElm();
-  await waitForElm(".zw-pageContainer");
+  const domPages = await waitForElm(".zw-pagecontainer");
+  while (domPages.length < totalPage) {
+    domPages = await waitForElm(".zw-pagecontainer");
+  }
+  console.log(domPages);
 });
 
 // const writerContent = Array.from(
@@ -57,3 +62,23 @@ window.addEventListener("load", async (e) => {
 //     .reduce((acc, paraContent) => acc + "\n" + paraContent, "");
 
 //   console.log(writerContent);
+
+// const delay = (sec) => new Promise(res => setTimeout(() => res(), sec));
+
+// (async() => {
+//     let counter = 0;
+
+//     const textContent = [];
+
+//     let pages = document.querySelectorAll('.zw-pagecontainer');
+
+//     while(counter < pages.length){
+//         pages[counter].scrollIntoView();
+//         await delay(1000);
+//         pages[counter].querySelectorAll('.zw-paragraph').forEach(paraDiv => textContent.push(paraDiv.textContent));
+//         counter++;
+//         pages = document.querySelectorAll('.zw-pagecontainer');
+//     }
+
+//     console.log(textContent);
+// })();
